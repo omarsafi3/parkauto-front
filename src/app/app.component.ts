bd2655f355ead3e96d84b8636800c64a1e781d29
omarsafi3 <safiomar@isimsf.u-sfax.tn>
2024-07-04 21:17:04 +0100

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parkauto';
+  showLoginLink: boolean = true; // Initially show the login link
+
+  constructor(private router: Router) {
+    // Listen to router events to detect navigation to login page
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && event.url === '/login') {
+        // Hide the login link if navigation ends at /login
+        this.showLoginLink = false;
+      }
+    });
+  }
 }
